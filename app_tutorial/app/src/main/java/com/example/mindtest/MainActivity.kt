ed7b7e6594fe299db64dab89d64c409db583b2f6
leechulhan <dhfhfkwhdk@naver.com>
2022-10-25 23:57:50 +0900

    Android Tutorial Commit

--- app_tutorial/app/src/main/java/com/example/mindtest/MainActivity.kt
+++ app_tutorial/app/src/main/java/com/example/mindtest/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.mindtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 
 class MainActivity : AppCompatActivity() {
+
+    // lateinit 키워드는 나중에 정의 하겠다는 키워드는
+    lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // TODO 강의 37:38 에 
+        // TODO navController 정의가 안됨 nav_host_fragment 를 찾지 못함
+        // TODO navController = nav_host_fragment
+
     }
 }

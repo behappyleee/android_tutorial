0456b16f5c543794df5c411e2661e26ac670472e
leechulhan <dhfhfkwhdk@naver.com>
2022-10-27 23:53:09 +0900

    Android 학습 Commit

--- app_tutorial/app/src/main/java/com/example/mindtest/MainActivity.kt
+++ app_tutorial/app/src/main/java/com/example/mindtest/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.mindtest
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,9 +16,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO 강의 37:38 에 
+        // TODO 강의 37:38 에
         // TODO navController 정의가 안됨 nav_host_fragment 를 찾지 못함
         // TODO navController = nav_host_fragment
-
+        
+        // kotlin 은 findViewId 를 사용할 필요가 없읍
+        navController = nav_host_fragment.findNavController();
+        
+        
+        
+        
+        
     }
 }

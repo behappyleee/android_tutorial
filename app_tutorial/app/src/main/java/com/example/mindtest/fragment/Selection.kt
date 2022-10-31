ec39446b828546056b3142013dacf0673993aa40
leechulhan <dhfhfkwhdk@naver.com>
2022-10-31 23:59:32 +0900

    Kotlin Android

--- app_tutorial/app/src/main/java/com/example/mindtest/fragment/Selection.kt
+++ app_tutorial/app/src/main/java/com/example/mindtest/fragment/Selection.kt
@@ -5,17 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.mindtest.R
+import kotlinx.android.synthetic.main.fragment_selection.*
 
+class Selection : Fragment(), View.OnClickListener {
 
-
-
-class Selection : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-
-    }
+    lateinit var navController: NavController;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,23 +22,17 @@ class Selection : Fragment() {
         return inflater.inflate(R.layout.fragment_selection, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Selection.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Selection().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view);
+
+        btn_back.setOnClickListener(this);
+        
     }
+
+    override fun onClick(p0: View?) {
+
+    }
+
 }
